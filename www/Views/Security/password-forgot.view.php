616ab6c2f3b075c35e48edf7434886f8bddff5c7
ludovicMAK <ludovic93mak@gmail.com>
2024-01-26 14:41:50 +0100

    mise en place du mot de passe oublier et correction de quelque fonction

--- www/Views/Security/password-forgot.view.php
+++ www/Views/Security/password-forgot.view.php
@@ -1,2 +1,5 @@
 Formulaire de réintialisation du mot de passe
 <?php $this->includeComponent("form", $configForm, $errorsForm);
+if (isset($this->data["message"])) {
+    echo "<h3>" . $this->data["message"] . "</h3>";
+}
--- www/Views/Security/register.view.php
+++ www/Views/Security/register.view.php
@@ -1,2 +1,6 @@
 Formulaire d'inscription
-<?php $this->includeComponent("form", $configForm, $errorsForm); ?>
+<?php $this->includeComponent("form", $configForm, $errorsForm);
+if (isset($this->data["message"])) {
+    echo "<h3>" . $this->data["message"] . "</h3>";
+}
+?>
