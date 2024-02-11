0d2487fd1767be768e38b9222033a7072642de4c
ludovicMAK <ludovic93mak@gmail.com>
2024-02-11 16:12:17 +0100

    création d'un du système de .env, et de la classe mail et de la fonction send, tâche fini

--- www/Core/Mail.php
+++ www/Core/Mail.php
@@ -10,8 +10,6 @@ class Mail
     private $phpmailer;
     public function __construct()
     {
-
-
         $this->phpmailer = new PHPMailer();
         $this->phpmailer->isSMTP();
         $this->phpmailer->Host = $_ENV["HOSTMAILTRAP"];
@@ -19,23 +17,6 @@ class Mail
         $this->phpmailer->Port = $_ENV["PORTMAILTRAP"];
         $this->phpmailer->Username = $_ENV["USERNAMEMAILTRAP"];
         $this->phpmailer->Password = $_ENV["PASSWORDMAILTRAP"];
-        $this->phpmailer->CharSet = "utf-8";
-        $this->phpmailer->SMTPDebug = SMTP::DEBUG_SERVER;
-        var_dump($this->phpmailer);
-
-        // $this->phpmailer = new PHPMailer();
-        // $this->phpmailer->isSMTP();
-        // $this->phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-        // var_dump($this->phpmailer);
-        // $this->phpmailer->SMTPAuth = true;
-        // $this->phpmailer->Port = 2525;
-        // $this->phpmailer->Username =
-        //     'db128c125df09d';
-        // $this->phpmailer->Password =
-        //     'ab4b6f5ce89761';
-        // $this->phpmailer->CharSet = "utf-8";
-        // $this->phpmailer->SMTPDebug = SMTP::DEBUG_SERVER;
-        // var_dump($phpmailer->Host, $this->phpmailer->Host);
     }
     public function sendMail(array $arrayMail, string $subject, string $contentMail)
     {
@@ -48,27 +29,11 @@ class Mail
             $this->phpmailer->isHTML(true);
             $this->phpmailer->Subject = $subject;
             $this->phpmailer->Body = $contentMail;
-            $this->phpmailer->send();
-            return "Email bien envoyé";
+            $message = $this->phpmailer->send() ? "Email bien envoyé" : "Le mail n'a pas pu être envoyé";
+            return $message;
         } else {
-            return "On n'a pas pu envoyer";
+            return "On n'a pas pu envoyer, un des mails fournie n'est pas valid";
         }
-        // $phpmailer = new PHPMailer();
-        // $phpmailer->isSMTP();
-        // $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-        // $phpmailer->SMTPAuth = true;
-        // $phpmailer->Port = 2525;
-        // $phpmailer->Username = 'db128c125df09d'; //changer
-        // $phpmailer->Password = 'ab4b6f5ce89761'; //changer
-
-        // $phpmailer->CharSet = "utf-8";
-        // $phpmailer->addAddress("ludovic93mak@gmail.com");
-        // $phpmailer->setFrom("no-reply@easyCook.fr");
-        // $phpmailer->isHTML(true);
-        // $phpmailer->Subject = "Vérification du compte";
-        // $phpmailer->Body    = 'Cliquez sur le lien suivant pour activer votre compte : <a href="http://localhost/change-password?token=">Activer</a>';
-        // $phpmailer->send();
-        // $message = 'Un e-mail d\'activation a été envoyé à votre adresse.';
     }
     private function arrayIsValid(array $arrayMail): bool
     {
