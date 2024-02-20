71d087907c5c2389d6874cf3fcda1933fbe09e68
Mijosed <mijosed@gmail.com>
2024-02-20 03:51:01 +0100

    CRUD page complet + PageBuilder ( manque implementationdes pages sur le front )

--- www/Views/Admin/pages.view.php
+++ www/Views/Admin/pages.view.php
@@ -5,34 +5,10 @@
     <a href="/admin/pages/add-page" class="button button-primary">Ajouter une page</a>
     <br>
     <br>
-
-    <p>Vous trouverez ci-dessous la liste des pages du site.</p>
-    <br>
-
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Title</th>
-               
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($pages as $page) : ?>
-                <tr>
-                    <td><?= $page->getId() ?></td>
-                    <td><?= $page->getTitle_page() ?></td>
-                    
-                    <td>
-                        <a href="/admin/pages/edit/<?= $page->getId() ?>">Modifier</a>
-                        |
-                        <a href="/admin/pages/delete/<?= $page->getId() ?>">Supprimer</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-
+    <?php $this->includeComponent("table", $configTable, $data);
+    if (isset($this->data["message"])) {
+        echo "<h3>" . $this->data["message"] . "</h3>";
+    }
+    ?>
     
 </section>
