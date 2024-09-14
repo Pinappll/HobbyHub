1746ee4f8266e6a11a3d742e851b54bb5f634ae0
Ludovic MAK <ludovic93mak@gmail.com>
2024-09-14 12:11:14 +0200

    save

--- www/Forms/Page/PageInsert.php
+++ www/Forms/Page/PageInsert.php
@@ -7,6 +7,8 @@ class PageInsert
 
     public function getConfig(array $data = []): array
     {
+        $navigations_id = isset($data['navigations_id']) ? $data['navigations_id'] : null;
+        $navigations = isset($data['navigations']) ? $data['navigations'] : null;
         return [
             "config" => [
                 "method" => "POST",
@@ -18,7 +20,11 @@ class PageInsert
             ],
             "inputs" => [
                 "title_page" => ["type" => "text", "class" => "input-form", "placeholder" => "Titre de page", "minlen" => 2, "required" => true, "error" => "Le titre doit faire plus de 2 caractères", "id" => "title-input'"],
-                "select-url" => ["type" => "select", "class" => "input-form", "placeholder" => "url de la page", "minlen" => 2, "required" => true, "error" => "Pas d'url de page"],
+                "name" => ["type" => "text", "class" => "input-form", "placeholder" => "Nom", "required" => true, "error" => "Votre nom doit faire plus de 2 caractères"],
+                "link" => ["type" => "text", "class" => "input-form", "placeholder" => "Lien", "required" => true, "error" => "Votre lien doit faire plus de 2 caractères"],
+                "position" => ["type" => "number", "class" => "input-form", "placeholder" => "Position", "required" => true, "error" => "Votre position doit être un nombre"],
+                "parent_id" => ["type" => "select", "class" => "input-form", "placeholder" => "Parent", "required" => true, "error" => "Votre parent doit être un nombre", "options" => $navigations, "value" => $navigations_id], 
+                "level" => ["type" => "number", "class" => "input-form", "placeholder" => "Niveau", "required" => true, "error" => "Votre niveau doit être un nombre"],
                 "content_page" => ["type" => "hidden", "class" => "contentInput", "placeholder" => "nom de la catégorie de recette", "minlen" => 2, "required" => true, "error" => "Pas de contenu de page"],
             ]
         ];
