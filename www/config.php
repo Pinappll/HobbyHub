06b1d33108118a6e9fd4da45db2c41540e3ced24
Mijosed <mijosed@gmail.com>
2024-09-15 04:23:30 +0200

    Ajout de www/config.php au .gitignore

--- www/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbHost = 'db-easycook-do-user-15878348-0.h.db.ondigitalocean.com';
-$dbPort = 25060;                                                      
-$dbName = 'easycook';                                                 
-$dbUser = 'doadmin';                                                                                
-$sslMode = 'require';   
