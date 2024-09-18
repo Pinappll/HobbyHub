d7473f4bd05fd8b859a89b1010abfd49331caa90
Joshua Grandville <joshua77130@live.fr>
2024-09-18 16:40:46 +0200

    installeur

--- www/Views/Components/form.php
+++ www/Views/Components/form.php
@@ -1,6 +1,8 @@
+
 <form method="<?= $config["config"]["method"] ?? "GET" ?>" action="<?= $config["config"]["action"] ?? "" ?>" class="<?= $config["config"]["class"] ?? "" ?>" id="<?= $config["config"]["id"] ?? "" ?>" <?= (!empty($config["config"]["enctype"])) ? 'enctype="' . $config["config"]["enctype"] . '"' : "" ?>>
-
-
+    <?php if (!empty($config["config"]["title"])) : ?>
+        <h1 class="form-title"><?= $config["config"]["title"] ?></h1>
+    <?php endif; ?>
     <div class="container">
         <?php if (!empty($data)) : ?>
             <div style="background-color: red">
@@ -9,7 +11,6 @@
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
-
 
         <?php foreach ($config["inputs"] as $name => $configInput) : ?>
             <div class="form-group">
@@ -29,22 +30,27 @@
                         <?php if ($name === "parent_id") : ?>
                             <option value="0">Aucun parent</option>
                         <?php endif; ?>
-
                     </select><br>
                 <?php elseif ($configInput["type"] === "partiel") : ?>
                     <div class="<?= $configInput["class"] ?>">
                         <h2><?= $configInput["titre"] ?></h2>
                     </div>
+                <?php elseif ($configInput["type"] === "color") : ?>
+                    <!-- Gestion du type color -->
+                    <label for="<?= $name ?>"><?= $configInput["label"] ?? "" ?></label>
+                    <input name="<?= $name ?>" type="color" id="<?= $name ?>" class="<?= $configInput["class"] ?? "" ?>" value="<?= $configInput["value"] ?? "#000000" ?>" <?= (!empty($configInput["required"])) ? "required" : "" ?>><br>
                 <?php else : ?>
                     <input name="<?= $name ?><?= ($configInput["type"] === "text" && !empty($configInput["multiple"])) ? "[]" : "" ?>" type="<?= $configInput["type"] ?? "text" ?>" id="<?= $configInput["id"] ?? "" ?>" class="<?= $configInput["class"] ?? "" ?>" placeholder="<?= $configInput["placeholder"] ?? "" ?>" value="<?= $configInput["value"] ?? "" ?>" <?= (!empty($configInput["required"])) ? "required" : "" ?>><br>
                 <?php endif; ?>
             </div>
         <?php endforeach; ?>
-        <? if (!empty($config["div"])) : ?>
+
+        <?php if (!empty($config["div"])) : ?>
             <div class="<?= $config["div"]["class"] ?? "" ?>">
                 <h2><?= $config["div"]["titre"] ?? "" ?></h2>
             </div>
-        <? endif; ?>
+        <?php endif; ?>
+        
         <input type="submit" class="button button-primary" value="<?= $config["config"]["submit"] ?? "Envoyer" ?>">
     </div>
 </form>
