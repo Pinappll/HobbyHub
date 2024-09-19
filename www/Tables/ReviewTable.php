30119763f0a192c0f4afe419a0ae7ebf9ded5180
Ludovic MAK <ludovic93mak@gmail.com>
2024-09-20 00:17:14 +0200

    ajout modération review

--- www/Tables/ReviewTable.php
+++ www/Tables/ReviewTable.php
@@ -13,7 +13,13 @@ class ReviewTable
             ["name" => "id_page_review", "title" => "Id Page"],
             ["name" => "content_review", "title" => "Contenu"],
             ["name" => "status_review", "title" => "Status"],
-            ["name" => "edit", "title" => "Editer", "type" => "edit", "route" => "/admin/reviews/edit?id_review="],
+            [
+                "name" => "validation",
+                "class" => "td-validation-review",
+                "title" => "Validation",
+                "method" => ["true" => "POST", "false" => "POST"],
+                "action" => ["true" => "/admin/reviews/validate", "false" => "/admin/reviews/refuse"]
+            ]
         ];
     }
 }
--- www/Views/Components/table.php
+++ www/Views/Components/table.php
@@ -2,7 +2,7 @@
     <thead>
         <tr>
             <?php foreach ($config as $column) : ?>
-                <th><?= $column["title"] ?></th>
+                <th><?= htmlspecialchars($column["title"]) ?></th>
             <?php endforeach; ?>
         </tr>
     </thead>
@@ -11,16 +11,31 @@
             <tr>
                 <?php foreach ($config as $column) : ?>
                     <?php if ($column["name"] === "image_url_recipe") : ?>
-                        <td><img src="<?= "/" . $row[$column["name"]] ?>" alt="image de la recette" class="table-image"></td>
+                        <td><img src="<?= "/" . htmlspecialchars($row[$column["name"]]) ?>" alt="image de la recette" class="table-image"></td>
                     
                     <?php elseif ($column["name"] === "edit") : ?>
-                        <td><a href="<?= $column["route"] .  $row["id"] ?>" class="button button-secondary">Éditer</a></td>
+                        <td><a href="<?= htmlspecialchars($column["route"] .  $row["id"]) ?>" class="button button-secondary">Éditer</a></td>
                     
                     <?php elseif ($column["name"] === "delete") : ?>
-                        <td><a href="<?= $column["route"] .  $row["id"] ?>" class="button button-danger">Supprimer</a></td>
+                        <td><a href="<?= htmlspecialchars($column["route"] .  $row["id"]) ?>" class="button button-danger">Supprimer</a></td>
+
+                    <?php elseif ($column["name"] === "validation") : ?>
+                        <td class="<?= htmlspecialchars($column["class"]) ?>">
+                            <!-- Formulaire pour refuser -->
+                            <form method="<?= htmlspecialchars($column["method"]["false"]) ?>" action="<?= htmlspecialchars($column["action"]["false"]) ?>" class="no-style-form">
+                                <input type="hidden" name="id" value="<?= htmlspecialchars($row["id"]) ?>">
+                                <input type="submit" value="Refusé" class="button button-danger">
+                            </form>
+
+                            <!-- Formulaire pour valider -->
+                            <form method="<?= htmlspecialchars($column["method"]["true"]) ?>" action="<?= htmlspecialchars($column["action"]["true"]) ?>" class="no-style-form">
+                                <input type="hidden" name="id" value="<?= htmlspecialchars($row["id"]) ?>">
+                                <input type="submit" value="Validé" class="button button-secondary">
+                            </form>
+                        </td>
                     
                     <?php else : ?>
-                        <td><?= $row[$column["name"]] ?></td>
+                        <td><?= htmlspecialchars($row[$column["name"]]) ?></td>
                     <?php endif; ?>
                 <?php endforeach; ?>
             </tr>
