9a78144a615a6a26b522fb098fdec6ebf6077a76
Joshua Grandville <joshua77130@live.fr>
2024-09-19 21:58:00 +0200

    couleur theme

--- www/Views/Admin/setting.view.php
+++ www/Views/Admin/setting.view.php
@@ -1,13 +1,11 @@
 <section class="section">
-
     <?php
     // Inclure le formulaire
-    $this->includeComponent("form", $configForm, $errorsForm);
+    $this->includeComponent("form", $configForm, $errorsForm ?? []);
     
     // Afficher un message s'il existe
     if (isset($this->data["message"])) {
         echo "<h3>" . $this->data["message"] . "</h3>";
     }
     ?>
-    
 </section>
