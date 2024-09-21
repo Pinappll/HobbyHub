f7b9b39a8db731f1443d946de30406cc80621f15
Mijosed <mijosed@gmail.com>
2024-09-21 04:35:40 +0200

    back_button

--- www/Views/Admin/add-recipe.view.php
+++ www/Views/Admin/add-recipe.view.php
@@ -4,7 +4,8 @@
     <br>
     <a href="/admin/recipes" class="button button-primary">Retour</a>
     <br>
-    <br><?php $this->includeComponent("form", $configForm, $errorsForm);
+    <br>
+    <?php $this->includeComponent("form", $configForm, $errorsForm);
     if (isset($this->data["message"])) {
         echo "<h3>" . $this->data["message"] . "</h3>";
     }
--- www/Views/Admin/menu/edit-menu.view.php
+++ www/Views/Admin/menu/edit-menu.view.php
@@ -1,4 +1,10 @@
+<section class="section">
 
+    <h1>Modifier menu</h1>
+    <br>
+    <a href="/admin/menus" class="button button-primary">Retour</a>
+    <br>
+    <br>
 <?php $this->includeComponent("form", $configForm, $errorsForm);
 if (isset($this->data["message"])) {
     echo "<h3>" . $this->data["message"] . "</h3>";
